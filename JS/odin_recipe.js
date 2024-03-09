ce931aa8f2d0c779f04a419fed915275250ddc3b
Taonga Jere <jeretaonga.tj@gmail.com>
2024-03-09 18:54:49 +0200

    modified popup window

--- JS/odin_recipe.js
+++ JS/odin_recipe.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     // Add event listener to the button
     document.getElementById("newsletter_button").addEventListener("click", function() {
         // Open a new popup window with the specified URL
-        var popup = window.open("forms/form.html", "Popup", "width=600,height=400");
+        var popup = window.open("forms/form.html", "Popup", "width=450,height=650");
     });
 });
 
